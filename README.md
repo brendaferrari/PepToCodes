a02d60619db0a5b98d896518beb90f6e763a6172
brendaferrari <brendafferrari@gmail.com>
2021-07-10 16:19:32 -0300

    version 1.0 beta

--- README.md
+++ README.md
@@ -8,11 +8,24 @@ This script was written with Python
 
 * Download the code and unzip it on the desirable directory
 
-* Change the smiles on line 2
-
 To run use the following command:
 ```
 python peptomon.py
+```
+
+* Type your peptide smiles as an input
+
+i.e. N[C@@]([H])(CCCNC(=N)N)C(=O)N[C@@]([H])([C@]([H])(O)C)C(=O)N[C@@]([H])(CCCCN)C(=O)N[C@@]([H])(CCCNC(=N)N)C(=O)O
+
+* The answer will pop-up at your terminal screen
+
+i.e. RTKR
+
+## Observations
+
+* In this version you may also obtain the 3 letter code for your smiles, just change line 10 to:
+```
+dictio = pd.read_csv("resources/codes.csv", sep=' ', names=['name', 'smiles', '3lcode', '1lcode'], usecols= ['smiles', '3lcode'], index_col=0, header=None, squeeze=True).to_dict()
 ```
 
 * **In this version is only possible to tranform ONE smiles at a time**
--- peptomon.py
+++ peptomon.py
@@ -1,31 +1,15 @@
-#Add the smiles in here
-smiles = 'N[C@@]([H])(CCCNC(=N)N)C(=O)N[C@@]([H])([C@]([H])(O)C)C(=O)N[C@@]([H])(CCCCN)C(=O)N[C@@]([H])(CCCNC(=N)N)C(=O)O'
+smiles = input("Type your peptide smiles: ")
+#N[C@@]([H])(CCCNC(=N)N)C(=O)N[C@@]([H])([C@]([H])(O)C)C(=O)N[C@@]([H])(CCCCN)C(=O)N[C@@]([H])(CCCNC(=N)N)C(=O)O
 
-#Here is the dictionary with your smiles and respective code
-dictio = {
-  "N[C@@]([H])(C)C(=O)": "A",
-  "N[C@@]([H])(CC(=O)N)C(=O)": "N",
-  "N[C@@]([H])(CC(=O)O)C(=O)": "D",
-  "N[C@@]([H])(CS)C(=O": "C",
-  "N[C@@]([H])(CCC(=O)N)C(=O)": "Q",
-  "NCC(=O)": "G",
-  "N[C@@]([H])(CCC(=O)O)C(=O)": "E",
-  "N[C@@]([H])(CC1=CN=C-N1)C(=O)": "H",
-  "N[C@@]([H])([C@]([H])(CC)C)C(=O)": "I",
-  "N[C@@]([H])(CC(C)C)C(=O)": "L",
-  "N[C@@]([H])(CCSC)C(=O)": "M",
-  "N[C@@]([H])(Cc1ccccc1)C(=O)": "F",
-  "N1[C@@]([H])(CCC1)C(=O)": "P",
-  "N[C@@]([H])(CO)C(=O)": "S",
-  "N[C@@]([H])(CC(=CN2)C1=C2C=CC=C1)C(=O)": "W",
-  "N[C@@]([H])(Cc1ccc(O)cc1)C(=O)": "Y",
-  "N[C@@]([H])(C(C)C)C(=O)": "V",
-  "N[C@@]([H])(CCCCN)C(=O)": "K",
-  "N[C@@]([H])(CCCNC(=N)N)C(=O)": "R",
-  "N[C@@]([H])([C@]([H])(O)C)C(=O)": "T",
-}
+import csv
+import pandas as pd
 
-#Here is the code
+# Here is the dictionary with your smiles and respective code
+dictio = pd.read_csv("resources/codes.csv", sep=' ', names=['name', 'smiles', '3lcode', '1lcode'], usecols= ['smiles', '1lcode'], index_col=0, header=None, squeeze=True).to_dict()
+
+#print(dictio)
+
+# Here is the code
 ## empty code list
 aacode = []
 i = 0
@@ -46,6 +30,6 @@ while i < len(smiles):
     if j == len(dictio.keys())-1:
       i = i+1
 
-#Here is to print the result
-# print(aacode)
-print(''.join(aacode))
+# Here is to print the result
+#print(aacode)
+print("Your code is:", ''.join(aacode))
