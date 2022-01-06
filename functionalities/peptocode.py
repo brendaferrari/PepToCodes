d75ec9ba7cf2c78154d60268f0e86b6dfc26f39b
brendaferrari <brendafferrari@gmail.com>
2022-01-06 11:02:39 -0300

    refactor: first commit

---
+++ functionalities/peptocode.py
@@ -0,0 +1,48 @@
+class PeptoCode:
+    """A class that represents peptide to code functionalities.
+    
+    Arguments:
+        smiles(str): smiles strings to convert to peptide code.
+        peptide_data(dict): data used to transform smiles code to peptide code.
+        
+    Methods:
+        count_and_change(self): Return a string with the aminoacid code."""
+
+    def __init__(self, smiles, peptide_data):
+        """Initialize the instance of a class.
+        
+        Arguments:
+            smiles(str): smiles strings to convert to peptide code.
+            peptide_data(dict): data used to transform smiles code to peptide code."""
+        self.smiles = smiles
+        self._peptide_data = peptide_data
+
+    @property
+    def peptide_data(self):
+        """Data used to transform smiles code to peptide code."""
+        return self._peptide_data
+        
+    def count_and_change(self):
+        """Return a string with the aminoacid code."""
+
+        aacode = []
+        i = 0
+        
+        while i < len(self.smiles):
+            keys = list(self.peptide_data.keys())
+
+            for j in range(len(keys)):
+                key = keys[j]
+
+                sub = self.smiles[i:i+len(key)]
+                if sub in self.peptide_data:
+                    i = i + len(key)
+                    aacode.append(self.peptide_data[sub])
+                    break
+
+                if j == len(self.peptide_data.keys())-1:
+                    i = i+1
+
+                code = ''.join(aacode)
+        
+        return code
---
+++ main.py
@@ -0,0 +1,62 @@
+from functionalities.peptocode import PeptoCode
+
+import pandas as pd
+
+
+print('Do you wish to transform one aminoacid of a file? Type a to one aminoacid or b for a file.')
+answer = input()
+if answer.lower() == "a":
+    print()
+    smiles = input('Type your peptide smiles: ')
+
+    dictio = pd.read_csv("resources/codes.csv", sep=' ', names=['name', 'smiles', '3lcode', '1lcode'], usecols= ['smiles', '1lcode'], index_col=0, header=None, squeeze=True).to_dict()
+
+    peptocode = PeptoCode(smiles, dictio)
+    aacode = peptocode.count_and_change()
+
+    print(f"Your code is: {aacode}")
+
+elif answer.lower() == "b":
+    smiles = input('Type your file name with path: ')
+
+    #do a dictionary instead of list
+    #https://stackoverflow.com/questions/36965507/writing-a-dictionary-to-a-text-file
+    dictio = pd.read_csv("resources/codes.csv", sep=' ', names=['name', 'smiles', '3lcode', '1lcode'], usecols= ['smiles', '1lcode'], index_col=0, header=None, squeeze=True).to_dict()
+    
+    df = pd.read_csv('smiles.txt')
+    code = {}
+    count = 0
+    for row in df.itertuples():
+        print(row)
+        print()
+        peptocode = PeptoCode(row[1], dictio)
+        aacode = peptocode.count_and_change()
+        print(aacode)
+        print()
+        code[row[1]] = []
+        code[row[1]].append(aacode)
+        print(code)
+        count += 1
+        print(count)
+    print(code)
+    print(count)
+        
+
+        # df = pd.DataFrame.from_dict(code, orient='index')
+        # print(df)
+
+# with open('smiles_output.txt','w') as data:
+#     data.write(str(code))
+# data.close()
+
+# import json
+
+
+# with open('file.txt', 'w') as file:
+#     for element in code:
+#         file.write(json.dumps(element))
+
+# with open("smiles_output.txt", "w") as textfile:
+#     for element in code:
+#         textfile.write(element)
+#     textfile.close()
