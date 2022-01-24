e3c151d4178553a91a5caf9a45b0867bfcb17116
brendaferrari <brendafferrari@gmail.com>
2022-01-24 18:13:37 -0300

    feat: add error message in not code

--- functionalities/peptocode.py
+++ functionalities/peptocode.py
@@ -26,23 +26,54 @@ class PeptoCode:
         """Return a string with the aminoacid code."""
 
         aacode = []
+        notaaCode = []
         i = 0
         
         while i < len(self.smiles):
             keys = list(self.peptide_data.keys())
-            # To add a * in not found, find a way to get j not in the range of the dictionary,  
-            # using for or other statements
+
             for j in range(len(keys)):
                 key = keys[j]
-
+                
                 sub = self.smiles[i:i+len(key)]
                 if sub in self.peptide_data:
                     i = i + len(key)
                     aacode.append(self.peptide_data[sub])
+                    break
 
                 if j == len(self.peptide_data.keys())-1:
-                    i = i+1
+                    notaaCode.append(sub)
+                    i = i + 1
+                    i = i + len(key)
+                    aacode.append('*')
 
-                code = ''.join(aacode)
+        code = ''.join(aacode)
+
+        return code, notaaCode
+
+    # def count_and_change(self):
+    #     """Return a string with the aminoacid code."""
+
+    #     aacode = []
+    #     i = 0
         
-        return code
+    #     while i < len(self.smiles):
+    #         keys = list(self.peptide_data.keys())
+    #         # To add a * in not found, find a way to get j not in the range of the dictionary,  
+    #         # using for or other statements
+            
+    #         for j in range(len(keys)):
+    #             key = keys[j]
+
+    #             sub = self.smiles[i:i+len(key)]
+    #             if sub in self.peptide_data:
+    #                 i = i + len(key)
+    #                 aacode.append(self.peptide_data[sub])
+    #                 break
+
+    #             if j == len(self.peptide_data.keys())-1:
+    #                 i = i+1
+
+    #             code = ''.join(aacode)
+        
+    #     return code
--- main.py
+++ main.py
@@ -13,9 +13,14 @@ if answer.lower() == "a":
     dictio = pd.read_csv("resources/codes.csv", sep=' ', names=['name', 'smiles', '3lcode', '1lcode'], usecols= ['smiles', '1lcode'], index_col=0, header=None, squeeze=True).to_dict()
 
     peptocode = PeptoCode(smiles, dictio)
-    aacode = peptocode.count_and_change()
+    aacode, notaaCode = peptocode.count_and_change()
 
     print(f"Your code is: {aacode}")
+
+    if notaaCode:
+        print(f"Some codes were not recognized: {notaaCode}")
+    else:
+        print(f"All codes were analyzed and recognized.")
 
 elif answer.lower() == "b":
     smiles = input('Type your file name with path: ')
@@ -23,6 +28,7 @@ elif answer.lower() == "b":
     
     df = pd.read_csv(smiles)
     aacode = []
+    notaaCode = []
     aasmiles = []
     countlen = []
     count = 0
@@ -30,16 +36,17 @@ elif answer.lower() == "b":
         aasmiles.append(row[1])
 
         peptocode = PeptoCode(row[1], dictio)
-        code = peptocode.count_and_change()
+        code, notCode = peptocode.count_and_change()
         aacode.append(code)
+        notaaCode.append(notCode)
 
         count += 1
         countlen.append(count)
 
-    variables = zip(aasmiles, aacode)
+    variables = zip(aasmiles, aacode, notaaCode)
     variablesDataframe = dict(zip(countlen, variables))
     print(variablesDataframe)
 
     dataframe = Dataframe(variablesDataframe)
-    data = dataframe.create_dataframe(columns=('smiles', 'code'))
+    data = dataframe.create_dataframe(columns=('smiles', 'code', 'not recognized'))
     data.to_csv('smiles_output.txt')
