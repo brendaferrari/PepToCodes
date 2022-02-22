440308355718a3840cdbe1cf0ebd3ca8e6357835
brendaferrari <brendafferrari@gmail.com>
2022-02-22 10:04:35 -0300

    refactor: implement database on main.py

--- functionalities/dictionary.py
+++ functionalities/dictionary.py
@@ -27,16 +27,16 @@ class Dictionary:
 
         return dictio
 
-    def dict_json(self, one_code=True):
+    def dict_json(self, three_code=True):
         """Return the full dictionary with all data regards aminoacids on norineDB database.
 
         Arguments:
-            one_code=False: Default is False. If true returns the one letter code of aminoacids
-            three_code=False: IN DEVELOPMENT
+            one_code=False: IN DEVELOPMENT 
+            three_code=False: Default is False. If true returns the three letter code of aminoacids
         """
 
-        if one_code is True:
-            with open("../resources/norineDB.json", "r") as r:
+        if three_code is True:
+            with open("resources/norineDB.json", "r") as r:
                 data = json.load(r)
 
                 canonicalSmiles = []
@@ -60,19 +60,24 @@ class Dictionary:
                         isomericSmiles.append(dataIsomeric)
                     
                     n += 1
-
-                smilesCanonical = dict(zip(codeCan, canonicalSmiles))
+                
+                smilesCanonical = dict(zip(canonicalSmiles, codeCan))
                 smilesIsomeric = dict(zip(codeIso, isomericSmiles))
+
+                dictio = {**smilesCanonical, **smilesIsomeric}
+
         
-        elif three_code is True:
+        elif one_code is True:
+            
             dictio = pd.read_csv("resources/codes.csv", sep=' ', names=['name', 'smiles', '3lcode', '1lcode'], 
                         usecols= ['smiles', '3lcode'], index_col=0, header=None, squeeze=True).to_dict()           
 
-        return smilesCanonical, smilesIsomeric
+        return dictio
 
-dictio = Dictionary()
+# dictio = Dictionary()
 
-smilesCanonical, smilesIsomeric = dictio.dict_json(True, False)
-print(f'Canonical: {smilesCanonical}')
-print(f'Isomeric: {smilesIsomeric}')
+# c = dictio.dict_json(True)
+# print(c)
+# print(f'Canonical: {smilesCanonical}')
+# print(f'Isomeric: {smilesIsomeric}')
 
